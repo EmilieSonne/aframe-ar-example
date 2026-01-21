7c9bc3e6aa08e77a129a7afe39d6e1b8f1655e91
Emilie Sonne Steinmann <estein19@student.aau.dk>
2026-01-21 11:04:51 +0100

    make snowman of different balls (bottom for start)

--- put-snowman.js
+++ put-snowman.js
@@ -7,12 +7,21 @@ AFRAME.registerComponent('put-snowman', {
             debugger;
             let p = e.detail.intersection.point;
             let scene = document.querySelector('a-scene');
-
             let snowman = document.createElement('a-entity');
-            snowman.setAttribute('geometry', 'primitive: sphere');
-            snowman.setAttribute('material', 'color: white');
-            snowman.setAttribute('scale', '1 1 1')
-            snowman.setAttribute('position', p);
+            let bottomball = document.createElement('a-sphere');
+            // position="0 1.25 -1" radius="1.25" color="blue"
+            bottomball.setAttribute('position', p);
+            bottomball.setAttribute('radius', '1.25');
+            bottomball.setAttribute('color', 'white');
+
+
+            //snowman.setAttribute('geometry', 'primitive: sphere');
+            //snowman.setAttribute('material', 'color: white');
+            //snowman.setAttribute('scale', '1 1 1')
+            //snowman.setAttribute('position', p);
+            snowman.setAttribute('shadow', {});
+            snowman.appendChild(botttomball);
+            
             scene.appendChild(snowman);
         }
 
