32b8c469edc853042e5f692eea255ede762124b3
Emilie Sonne Steinmann <estein19@student.aau.dk>
2026-01-21 10:57:26 +0100

    add sphere when put-snowman is called

--- put-snowman.js
+++ put-snowman.js
@@ -5,8 +5,15 @@ AFRAME.registerComponent('put-snowman', {
 
         this.addSnowman = function(e){
             debugger;
-            console.log(e);
-            debugger;
+            let p = e.detail.intersection.point;
+            let scene = document.querySelector('a-scene');
+
+            let snowman = document.createElement('a-entity');
+            snowman.setAttribute('geometry', 'primitive: sphere');
+            snowman.setAttribute('material', 'color: white');
+            snowman.setAttribute('scale', '1 1 1')
+            snowman.setAttribute('position', p);
+            scene.appendChild(snowman);
         }
 
         this.el.addEventListener('click', this.addSnowman);
