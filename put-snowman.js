a3a5c696378497939b9b6435e947ff6f4baa43ec
Emilie Sonne Steinmann <estein19@student.aau.dk>
2026-01-21 09:23:40 +0100

    try to add function on click

--- put-snowman.js
+++ put-snowman.js
@@ -1,8 +1,10 @@
 AFRAME.registerComponent('put-snowman', {
     init: function(){
+        debugger;
         let el = this.el; 
 
         this.addSnowman = function(e){
+            debugger;
             print(e);
             debugger;
         }
