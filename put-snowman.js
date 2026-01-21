f6e32c67bd50e03ce24c38f73fac317880ad7ddd
Emilie Sonne Steinmann <estein19@student.aau.dk>
2026-01-21 12:02:08 +0100

    do it simple...

--- put-snowman.js
+++ put-snowman.js
@@ -43,10 +43,39 @@ AFRAME.registerComponent('put-snowman', {
         this.addSnowmanOrPinetree = function(e){
             let x = Math.random();
             if (x < 0.7){
-                addPinetree(e);
+                let p = e.detail.intersection.point;
+                console.log(p);
+                let scene = document.querySelector('a-scene');
+                let pinetree = document.createElement('a-entity');
+                pinetree.setAttribute('geometry', 'primitive: cone');
+                pinetree.setAttribute('material', 'color: green');
+                pinetree.setAttribute('scale', '0.2 0.5 0.2');
+                pinetree.setAttribute('position', p);
+                pinetree.setAttribute('shadow', {});
+
+                scene.appendChild(pinetree);
             }
             else {
-                addSnowman(e);
+                let p = e.detail.intersection.point;
+                console.log(p);
+                let scene = document.querySelector('a-scene');
+                let snowman = document.createElement('a-entity');
+                snowman.setAttribute('geometry', 'primitive: sphere');
+                snowman.setAttribute('material', 'color: white');
+                snowman.setAttribute('scale', '0.2 0.2 0.2');
+                snowman.setAttribute('position', p);
+                snowman.setAttribute('shadow', {});
+
+                let snowman_top = document.createElement('a-entity');
+                snowman_top.setAttribute('geometry', 'primitive: sphere');
+                snowman_top.setAttribute('material', 'color: white');
+                snowman_top.setAttribute('scale', '0.15 0.15 0.15');
+                p.y += 0.3;
+                snowman_top.setAttribute('position', p);
+                // snowman_top.setAttribute('shadow', {});
+
+                scene.appendChild(snowman);
+                scene.appendChild(snowman_top);
             }
         }
 
